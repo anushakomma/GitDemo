9930c09040a5fbe20a25b8aa784dec910359ed4a
anusha <kommaanusha224@gmail.com>
2025-07-29 21:29:56 +0530

    architect asian development

--- demoBrowser.py
+++ demoBrowser.py
@@ -1,6 +1,10 @@
 import time
-from selenium import webdriver
 
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from FindElementsTest import countries
 
 driver = webdriver.Chrome()
 
@@ -10,9 +14,27 @@ print(driver.title)
 print(driver.current_url)
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome",help= "run slow test"
+    )
 
+@pytest.fixture(scope="function")
+def browserInstance(request):
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        driver = webdriver.Chrome()
+    elif browser_name =="firefox":
+        driver = webdriver.Firefox()
+    driver.implicitly_wait(5)
+    yield driver
 
-
+for country in countries:
+    if country.text == "India":
+        country.click()
+        break
+print(driver.find_element(By.ID,"autosuggest").get_attribute('value'))
+#assert driver.find_element(By.ID,"autosuggest").get_attribute('value')== "India"
 
 
 time.sleep(3)
